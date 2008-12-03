3574b8f8e115618d75520a55086f2dc5a516e9f8
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2008-12-03 07:41:30 +0000

    Add a test for proper handling of bone space translations.
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@50529 07c76cb3-cb09-0410-85de-c24d39f1912e

--- test/testPhysique.cpp
+++ test/testPhysique.cpp
@@ -1,7 +1,9 @@
 #include "TestPrologue.h"
 #include <cal3d/renderer.h>
+#include <cal3d/bone.h>
 #include <cal3d/model.h>
 #include <cal3d/submesh.h>
+#include <cal3d/skeleton.h>
 #include <cal3d/physique.h>
 #include <cal3d/mixer.h>
 
@@ -20,7 +22,7 @@ void setVertex(CalCoreSubmesh* submesh, unsigned vertexId, float x, float y, flo
     for (int i = 0; i < numInfluences; i++) {
         CalCoreSubmesh::Influence myInfluence;
         myInfluence.boneId = i;
-        myInfluence.weight = 1.0f;
+        myInfluence.weight = 1.0f / numInfluences;
         influences.push_back(myInfluence);
     }
 
@@ -92,25 +94,101 @@ TEST(getVerticesAndNormals_on_mesh_with_one_bone_generates_vertices) {
     CHECK_EQUAL(vertexBuffer[6], 0);
 }
 
-TEST(getVerticesAndNormals_on_mesh_with_two_bones_generates_normals_that_are_not_unit_vectors) {
+TEST(getVerticesAndNormals_on_mesh_with_two_translation_bones) {
     CalCoreMesh * coreMesh = createTestCoreMesh();
     CalCoreSubmesh * coreSubMesh = coreMesh->getCoreSubmesh(0);
     coreSubMesh->reserve(1, 0, 1);
     CalCoreSubmesh::LodData ld = {0, 0};
     setVertex(coreSubMesh, 0, 1, 2, 3, 1, 1, 0, 2, CalMakeColor(CalVector(1, 1, 1)), ld);
 
+    CalCoreBone* coreBone1 = new CalCoreBone;
+    coreBone1->createWithName("bone1");
+    coreBone1->setTranslation(CalVector(1, 0, 0));
+    coreBone1->setTranslationBoneSpace(CalVector(-1, 0, 0));
+
+    CalCoreBone* coreBone2 = new CalCoreBone;
+    coreBone2->createWithName("bone2");
+    coreBone1->setTranslation(CalVector(0, 1, 0));
+    coreBone1->setTranslationBoneSpace(CalVector(0, -1, 0));
+
+    CalCoreSkeleton* coreSkeleton = new CalCoreSkeleton;
+    coreSkeleton->addCoreBone(coreBone1);
+    coreSkeleton->addCoreBone(coreBone2);
+
     shared_ptr<CalCoreModel> coreModel(new CalCoreModel(), &caldestroy<CalCoreModel>);
     coreModel->createWithName("test");
     coreModel->addCoreMesh(coreMesh);
     coreModel->setCoreSkeleton(createTestCoreSkeleton(2)); // No coreSkeleton == model->create() returns false
 
-    shared_ptr<CalModel> model(createTestModel(coreModel));
+    shared_ptr<CalModel> model(new CalModel(), &caldestroy<CalModel>);
+    model->create(coreModel.get());
+    CHECK(model->attachMesh(0));
+
+    model->getMixer()->updateAnimation(0.0f);
+    model->getMixer()->updateSkeleton();
 
     CalSubmesh* sm = model->getMesh(0)->getSubmesh(0);
     CHECK_EQUAL(1, sm->getVertexCount());
     float vertexBuffer[8];
-
     CalPhysique::calculateVerticesAndNormals(model->getSkeleton(), sm, vertexBuffer);
-    CHECK_CLOSE(2.82842708, sqrt(pow(vertexBuffer[3], 2) + pow(vertexBuffer[4], 2) + pow(vertexBuffer[5], 2)), 0.0001f);
+
+    CHECK_EQUAL(1.0f, vertexBuffer[0]);
+    CHECK_EQUAL(2.0f, vertexBuffer[1]);
+    CHECK_EQUAL(3.0f, vertexBuffer[2]);
+
+    CHECK_EQUAL(1.0f, vertexBuffer[4]);
+    CHECK_EQUAL(1.0f, vertexBuffer[5]);
+    CHECK_EQUAL(0.0f, vertexBuffer[6]);
 }
 
+TEST(getVerticesAndNormals_on_mesh_with_two_translated_bones) {
+    CalCoreMesh * coreMesh = createTestCoreMesh();
+    CalCoreSubmesh * coreSubMesh = coreMesh->getCoreSubmesh(0);
+    coreSubMesh->reserve(1, 0, 1);
+    CalCoreSubmesh::LodData ld = {0, 0};
+    setVertex(coreSubMesh, 0, 1, 2, 3, 1, 1, 0, 2, CalMakeColor(CalVector(1, 1, 1)), ld);
+
+    CalCoreBone* coreBone1 = new CalCoreBone;
+    coreBone1->createWithName("bone1");
+    coreBone1->setTranslation(CalVector(2, 0, 0));
+    coreBone1->setTranslationBoneSpace(CalVector(-1, 0, 0));
+
+    CalCoreBone* coreBone2 = new CalCoreBone;
+    coreBone2->createWithName("bone2");
+    coreBone1->setTranslation(CalVector(0, 2, 0));
+    coreBone1->setTranslationBoneSpace(CalVector(0, -1, 0));
+
+    CalCoreSkeleton* coreSkeleton = new CalCoreSkeleton;
+    coreSkeleton->addCoreBone(coreBone1);
+    coreSkeleton->addCoreBone(coreBone2);
+
+    shared_ptr<CalCoreModel> coreModel(new CalCoreModel(), &caldestroy<CalCoreModel>);
+    coreModel->createWithName("test");
+    coreModel->addCoreMesh(coreMesh);
+    coreModel->setCoreSkeleton(createTestCoreSkeleton(2)); // No coreSkeleton == model->create() returns false
+
+    shared_ptr<CalModel> model(new CalModel(), &caldestroy<CalModel>);
+    model->create(coreModel.get());
+    CHECK(model->attachMesh(0));
+
+    model->getSkeleton()->getBone(0)->clearState();
+    model->getSkeleton()->getBone(1)->clearState();
+    model->getSkeleton()->getBone(0)->blendState(1.0f, CalVector(1, 0, 0), CalQuaternion(), 1.0f, true, 1.0f);
+    model->getSkeleton()->getBone(1)->blendState(1.0f, CalVector(0, 1, 0), CalQuaternion(), 1.0f, true, 1.0f);
+    model->getSkeleton()->lockState();
+    model->getSkeleton()->calculateState();
+    //model->getMixer()->updateSkeleton();
+
+    CalSubmesh* sm = model->getMesh(0)->getSubmesh(0);
+    CHECK_EQUAL(1, sm->getVertexCount());
+    float vertexBuffer[8];
+    CalPhysique::calculateVerticesAndNormals(model->getSkeleton(), sm, vertexBuffer);
+
+    CHECK_EQUAL(1.5f, vertexBuffer[0]);
+    CHECK_EQUAL(2.5f, vertexBuffer[1]);
+    CHECK_EQUAL(3.0f, vertexBuffer[2]);
+
+    CHECK_EQUAL(1.0f, vertexBuffer[4]);
+    CHECK_EQUAL(1.0f, vertexBuffer[5]);
+    CHECK_EQUAL(0.0f, vertexBuffer[6]);
+}
