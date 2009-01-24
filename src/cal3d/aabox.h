70af3308c134b6ddfa1b5d3efd8a68093c2b2a5f
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2009-01-24 05:49:51 +0000

    Fixing BuildBot: forgot this file
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@52521 07c76cb3-cb09-0410-85de-c24d39f1912e

---
+++ src/cal3d/aabox.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "cal3d/vector.h"
+
+#ifdef min
+#undef min
+#endif
+
+#ifdef max
+#undef max
+#endif
+
+struct CalAABox {
+    CalAABox()
+    {}
+
+    CalAABox(const CalVector& _min, const CalVector& _max)
+    : min(_min)
+    , max(_max)
+    {}
+
+    CalVector min;
+    CalVector max;
+};
+
+inline bool operator==(const CalAABox& lhs, const CalAABox& rhs) {
+    return lhs.min == rhs.min && lhs.max == rhs.max;
+}
