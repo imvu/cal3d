4d2f9d0b94da1c848e55b263305025298a672235
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2009-05-19 17:22:43 +0000

    I destroyed BuildBot.  Woops.  Adding missing files.  Fixing BuildBot.
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@58159 07c76cb3-cb09-0410-85de-c24d39f1912e

---
+++ plugins/src/PCH.h
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <sstream>
+#include <algorithm>
+
+#include <afxwin.h>
+#include <afxext.h>
+#include <afxcmn.h>
+
+#include "max.h"
+#include "maxscrpt/maxscrpt.h"
+#include "bipexp.h"
+#include "phyexp.h"
+#include "decomp.h"
+#include "utillib.h"
+#include "SimpObj.h"
+
+#if MAX_RELEASE >= 4000
+#include "iskin.h"
+#endif
+
+#include "cal3d/cal3d.h"
+#include "cal3d/coremodel.h"
+
+#include "resource.h"
