2424446bfd59dc28218e1caf5e5ce44045e15a87
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2009-05-19 10:13:16 +0000

    Fixing BuildBot: forgot these files
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@58144 07c76cb3-cb09-0410-85de-c24d39f1912e

---
+++ src/cal3d/PCH.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <fstream>
+#include <boost/shared_ptr.hpp>
+#include <boost/noncopyable.hpp>
+#include <assert.h>
