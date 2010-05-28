a12262ed058749bbbfe0551143144939483e704f
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2010-05-28 17:00:11 +0000

    update the script that builds cal3d for linux... but it doesn't compile.  there's actually a substantial amount of debt to fix once we need to update our cfl optimizer
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@79770 07c76cb3-cb09-0410-85de-c24d39f1912e

--- build-linux.sh
+++ build-linux.sh
@@ -7,16 +7,12 @@ SOURCES="
     src/cal3d/animation_cycle.cpp
     src/cal3d/bone.cpp
     src/cal3d/buffersource.cpp
-    src/cal3d/cal3d_wrapper.cpp
     src/cal3d/calxmlbindings.cpp
     src/cal3d/coreanimatedmorph.cpp
     src/cal3d/coreanimation.cpp
     src/cal3d/corebone.cpp
-    src/cal3d/corekeyframe.cpp
-    src/cal3d/corematerial.cpp
     src/cal3d/coremesh.cpp
     src/cal3d/coremodel.cpp
-    src/cal3d/coremorphanimation.cpp
     src/cal3d/coremorphkeyframe.cpp
     src/cal3d/coremorphtrack.cpp
     src/cal3d/coreskeleton.cpp
@@ -25,20 +21,17 @@ SOURCES="
     src/cal3d/coretrack.cpp
     src/cal3d/error.cpp
     src/cal3d/global.cpp
-    src/cal3d/hardwaremodel.cpp
     src/cal3d/loader.cpp
     src/cal3d/matrix.cpp
     src/cal3d/mesh.cpp
     src/cal3d/mixer.cpp
     src/cal3d/model.cpp
-    src/cal3d/morphtargetmixer.cpp
     src/cal3d/physique.cpp
     src/cal3d/platform.cpp
     src/cal3d/quaternion.cpp
     src/cal3d/renderer.cpp
     src/cal3d/saver.cpp
     src/cal3d/skeleton.cpp
-    src/cal3d/springsystem.cpp
     src/cal3d/streamsource.cpp
     src/cal3d/submesh.cpp
     src/cal3d/tinybind.cpp
@@ -49,4 +42,4 @@ SOURCES="
     src/cal3d/vector.cpp
     src/cal3d/xmlformat.cpp"
 
-g++ -Wl,--demangle -Wall -shared -Isrc -fpermissive -Wno-non-virtual-dtor -Wno-sign-compare -Wno-reorder -o cal3d.so $SOURCES
+g++ -Wl,--demangle -Wall -shared -Isrc -I/usr/include/boost-1_33_1 -fpermissive -Wno-non-virtual-dtor -Wno-sign-compare -Wno-reorder -o cal3d.so $SOURCES
