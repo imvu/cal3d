f4912063950109cd057b6a9c6a92ed07495f4049
chad <chad@07c76cb3-cb09-0410-85de-c24d39f1912e>
2011-09-20 05:49:06 +0000

    hugely simplify mesh scale
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@117781 07c76cb3-cb09-0410-85de-c24d39f1912e

--- src/cal3d/bonetransform.cpp
+++ src/cal3d/bonetransform.cpp
@@ -1,5 +1,6 @@
 #include "cal3d/bonetransform.h"
 #include "cal3d/matrix.h"
+#include "cal3d/transform.h"
 
 BoneTransform::BoneTransform(const CalVector4& rx, const CalVector4& ry, const CalVector4& rz)
     : rowx(rx)
@@ -8,6 +9,22 @@ BoneTransform::BoneTransform(const CalVector4& rx, const CalVector4& ry, const C
 {}
 
 BoneTransform::BoneTransform(const CalMatrix& matrix, const CalVector& translation) {
+    rowx.set(matrix.dxdx, matrix.dxdy, matrix.dxdz, translation.x);
+    rowy.set(matrix.dydx, matrix.dydy, matrix.dydz, translation.y);
+    rowz.set(matrix.dzdx, matrix.dzdy, matrix.dzdz, translation.z);
+}
+
+BoneTransform::BoneTransform(const cal3d::RotateTranslate& transform) {
+    CalMatrix matrix(transform.rotation);
+    const CalVector& translation = transform.translation;
+    rowx.set(matrix.dxdx, matrix.dxdy, matrix.dxdz, translation.x);
+    rowy.set(matrix.dydx, matrix.dydy, matrix.dydz, translation.y);
+    rowz.set(matrix.dzdx, matrix.dzdy, matrix.dzdz, translation.z);
+}
+
+BoneTransform::BoneTransform(const cal3d::Transform& transform) {
+    const CalMatrix& matrix = transform.basis;
+    const CalVector& translation = transform.translation;
     rowx.set(matrix.dxdx, matrix.dxdy, matrix.dxdz, translation.x);
     rowy.set(matrix.dydx, matrix.dydy, matrix.dydz, translation.y);
     rowz.set(matrix.dzdx, matrix.dzdy, matrix.dzdz, translation.z);
--- src/cal3d/bonetransform.h
+++ src/cal3d/bonetransform.h
@@ -3,6 +3,10 @@
 #include "cal3d/vector4.h"
 
 class CalMatrix;
+namespace cal3d {
+    struct RotateTranslate;
+    struct Transform;
+}
 
 // 3x3 transform matrix plus a translation 3-vector (stored in the w components
 // of the rows.  This struct needs to be 16-byte aligned for SSE.
@@ -10,6 +14,8 @@ struct CAL3D_API BoneTransform {
     BoneTransform() {}
     BoneTransform(const CalVector4& rx, const CalVector4& ry, const CalVector4& rz);
     BoneTransform(const CalMatrix& matrix, const CalVector& translation);
+    BoneTransform(const cal3d::RotateTranslate& transform);
+    BoneTransform(const cal3d::Transform& transform);
 
     CalVector4 rowx;
     CalVector4 rowy;
