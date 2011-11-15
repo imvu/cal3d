ec52f7844838f2732de7e3bae31a57b59b150b6c
kgeorge <kgeorge@07c76cb3-cb09-0410-85de-c24d39f1912e>
2011-11-15 21:09:03 +0000

    -fixed the crash when attachment products used an xrf asset from the bodypattern product -re-factored modeldefinitionbuildertest.py to use cal3d test data from a common fake test data file 'cal3d/meshtestdata.py' -started to re-factor requesthandlertest.py to use fake test data from 'cal3d/meshtestdata.py'
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@122066 07c76cb3-cb09-0410-85de-c24d39f1912e

---
+++ meshtestdata.py
@@ -0,0 +1,154 @@
+
+from imvu import libzero
+from imvu.avatarwindow import ModelTextureData
+
+class materialTestData:
+
+    material_template =\
+        "<HEADER MAGIC=\"XRF\" VERSION=\"919\" />"\
+        "<MATERIAL NUMMAPS=\"2\">"\
+        "<AMBIENT>149 149 149 0</AMBIENT>"\
+        "<DIFFUSE>255 255 255 255</DIFFUSE>"\
+        "<SPECULAR>229 229 229 0</SPECULAR>"\
+        "<SHININESS>0</SHININESS>"\
+        "<MAP TYPE=\"Diffuse Color\">%s</MAP>"\
+        "<MAP TYPE=\"Opacity\">%s</MAP>"\
+        "</MATERIAL>"
+
+
+    material_no_textures =\
+        "<HEADER MAGIC=\"XRF\" VERSION=\"919\" />"\
+        "<MATERIAL NUMMAPS=\"0\">"\
+        "<AMBIENT>149 149 149 0</AMBIENT>"\
+        "<DIFFUSE>255 255 255 255</DIFFUSE>"\
+        "<SPECULAR>229 229 229 0</SPECULAR>"\
+        "<SHININESS>0</SHININESS>"\
+        "</MATERIAL>"
+
+    material_without_opacity_template =\
+        "<HEADER MAGIC=\"XRF\" VERSION=\"919\" />"\
+        "<MATERIAL NUMMAPS=\"2\">"\
+        "<AMBIENT>149 149 149 0</AMBIENT>"\
+        "<DIFFUSE>255 255 255 255</DIFFUSE>"\
+        "<SPECULAR>229 229 229 0</SPECULAR>"\
+        "<SHININESS>0</SHININESS>"\
+        "<MAP TYPE=\"Diffuse Color\">%s</MAP>"\
+        "</MATERIAL>"
+
+    material_with_unsupported_template =\
+        "<HEADER MAGIC=\"XRF\" VERSION=\"919\" />"\
+        "<MATERIAL NUMMAPS=\"2\">"\
+        "<AMBIENT>149 149 149 0</AMBIENT>"\
+        "<DIFFUSE>255 255 255 255</DIFFUSE>"\
+        "<SPECULAR>229 229 229 0</SPECULAR>"\
+        "<SHININESS>0</SHININESS>"\
+        "<MAP TYPE=\"Diffuse Color\">%s</MAP>"\
+        "<MAP TYPE=\"Opacity\">%s</MAP>"\
+        "<MAP TYPE=\"Unsupported\">%s</MAP>"\
+        "</MATERIAL>"
+
+    
+
+    basecoat_material_template =\
+        "<HEADER MAGIC=\"XRF\" VERSION=\"919\" />"\
+        "<MATERIAL NUMMAPS=\"2\">"\
+        "<AMBIENT>149 149 149 0</AMBIENT>"\
+        "<DIFFUSE>255 255 255 255</DIFFUSE>"\
+        "<SPECULAR>229 229 229 0</SPECULAR>"\
+        "<SHININESS>0</SHININESS>"\
+        "<MAP TYPE=\"Diffuse Color\">%s</MAP>"\
+        "<MAP TYPE=\"Opacity\">%s</MAP>"\
+        "</MATERIAL>"
+
+   
+
+    def __init__(self):
+        self.stringToTextureMap = {}
+        self.diffuseMapName = "desert-camo-512.jpg"
+        self.opacityMapName = "512sq-opmap.jpg"
+        self.basecoatDiffuseMapName = "basecoat_diffuse.jpg"
+        self.basecoatOpacityMapName = "basecoat_opacity.jpg"        
+        self.material = materialTestData.material_template % (self.diffuseMapName, self.opacityMapName)        
+        self.material_without_opacity = materialTestData.material_without_opacity_template % self.diffuseMapName
+        self.material_with_unsupported = materialTestData.material_with_unsupported_template % (self.diffuseMapName, self.opacityMapName, "unsupported.jpg")
+        self.basecoat_material = materialTestData.basecoat_material_template % (self.basecoatDiffuseMapName, self.basecoatOpacityMapName) 
+
+    def setUp(self):  
+        self.stringToTextureMap = {}   
+
+    def defineTexture(self, name, modelTextureData):
+        self.stringToTextureMap[name] = modelTextureData
+
+    def defineTextureColor(self, name, r, g, b, size=libzero.Vec2i(1, 1)):
+        rgb = chr(r) + chr(g) + chr(b)
+        s = size.x * size.y * rgb
+        self.stringToTextureMap[name] = ModelTextureData(name, s, size.x, size.y, size.x, size.y, False)
+
+meshDataPrefix =\
+    "<HEADER MAGIC=\"XMF\" VERSION=\"919\" />"\
+    "<MESH NUMSUBMESH=\"1\">"
+
+submeshTemplate =\
+    "   <SUBMESH NUMVERTICES=\"3\" NUMFACES=\"1\" NUMLODSTEPS=\"0\" NUMSPRINGS=\"0\" NUMMORPHS=\"1\" NUMTEXCOORDS=\"1\" MATERIAL=\"%d\">"\
+    "       <VERTEX NUMINFLUENCES=\"1\" ID=\"0\">"\
+    "           <POS>5759.05 -1176.88 -0.00023478</POS>"\
+    "           <NORM>1.27676e-008 2.40249e-008 -1</NORM>"\
+    "           <COLOR>0 0 0</COLOR>"\
+    "           <TEXCOORD>0.99311 0.00973237</TEXCOORD>"\
+    "           <INFLUENCE ID=\"0\">1</INFLUENCE>"\
+    "       </VERTEX>"\
+    "       <VERTEX NUMINFLUENCES=\"2\" ID=\"1\">"\
+    "           <POS>-5759.05 -1176.88 -0.000413365</POS>"\
+    "           <NORM>1.55047e-008 -2.86491e-008 -1</NORM>"\
+    "           <COLOR>0 0 0</COLOR>"\
+    "           <TEXCOORD>0.99311 0.982444</TEXCOORD>"\
+    "           <INFLUENCE ID=\"0\">0.5</INFLUENCE>"\
+    "           <INFLUENCE ID=\"1\">0.5</INFLUENCE>"\
+    "       </VERTEX>"\
+    "       <VERTEX NUMINFLUENCES=\"1\" ID=\"2\">"\
+    "           <POS>-5759.05 -3274.86 -0.000507484</POS>"\
+    "           <NORM>1.11221e-008 6.89228e-008 -1</NORM>"\
+    "           <COLOR>0 0 0</COLOR>"\
+    "           <TEXCOORD>0.79062 0.982444</TEXCOORD>"\
+    "           <INFLUENCE ID=\"1\">1</INFLUENCE>"\
+    "       </VERTEX>"\
+    "       <MORPH NAME=\"ogle\" NUMBLENDVERTS=\"1\" MORPHID=\"0\">"\
+    "           <BLENDVERTEX VERTEXID=\"2\">"\
+    "               <POSITION>6000 -1500 0</POSITION>"\
+    "               <NORMAL>0 0.707 -0.707</NORMAL>"\
+    "               <TEXCOORD>0.0 0.25</TEXCOORD>"\
+    "           </BLENDVERTEX>"\
+    "       </MORPH>"\
+    "       <FACE VERTEXID=\"0 1 2\" />"\
+    "    </SUBMESH>"
+
+meshDataSuffix =\
+    "</MESH>"
+
+def makeMeshTestData(materialIds):
+    meshData = meshDataPrefix
+    for materialId in materialIds:
+        meshData += submeshTemplate % (materialId,)
+    meshData += meshDataSuffix
+    return meshData
+
+
+skeletonTestData = \
+"""<HEADER VERSION=\"910\" MAGIC="XSF" />
+    <SKELETON SCENEAMBIENTCOLOR="1 1 1" NUMBONES="2">
+    <BONE NAME="FooRoot" NUMCHILDS="1" ID="0">
+        <TRANSLATION>0 0 -30</TRANSLATION>
+        <ROTATION>0 0 0 1</ROTATION>
+        <LOCALTRANSLATION>0 0 30</LOCALTRANSLATION>
+        <LOCALROTATION>0 0 0 1</LOCALROTATION>
+        <PARENTID>-1</PARENTID>
+        <CHILDID>1</CHILDID>
+    </BONE>
+    <BONE NAME="FooChild" NUMCHILDS="0" ID="1">
+        <TRANSLATION>0 -10 500</TRANSLATION>
+        <ROTATION>0 0 0 1</ROTATION>
+        <LOCALTRANSLATION>0 10 -470</LOCALTRANSLATION>
+        <LOCALROTATION>0 0 0 1</LOCALROTATION>
+        <PARENTID>0</PARENTID>
+    </BONE>
+</SKELETON>"""
