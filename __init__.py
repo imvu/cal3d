0b41bbeae4e91eb9c3c21838ea23ea7f20fcd102
andy <andy@07c76cb3-cb09-0410-85de-c24d39f1912e>
2014-03-18 18:54:34 +0000

    Temp hack to fix the Mac test runners: There's an issue where the native cal3d extension does not provide definitions for some standard C++ symbols.  The avatarwindow extension does, so we can work around the issue by ensuring that avatarwindow is always imported first.
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@190330 07c76cb3-cb09-0410-85de-c24d39f1912e

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 import imvu
+import imvu.opsys
+
+if imvu.opsys.isMacOSX():
+    # HACK: Work around lame link dependencies.
+    # _avatarwindow provides some standard C++ stuff that
+    # _cal3d doesn't.
+    import imvu.avatarwindow
 
 if __debug__:
     from _cal3d_debug import *
