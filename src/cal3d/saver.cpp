a94fe5412122fe414dc5727394e69d70ac4fbe7e
dbrittain <dbrittain@07c76cb3-cb09-0410-85de-c24d39f1912e>
2014-05-15 21:34:44 +0000

    Updated to properly write out bone IDs, NUMCHILDS, texture verts (apparently left out from earlier port to tinyXML)
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@194983 07c76cb3-cb09-0410-85de-c24d39f1912e

--- src/cal3d/saver.cpp
+++ src/cal3d/saver.cpp
@@ -654,7 +654,7 @@ bool CalSaver::saveCoreSubmesh(std::ostream& os, CalCoreSubmesh* pCoreSubmesh) {
 
         // write all influences of this vertex
         for (; currentInfluence != nextVertex; ++currentInfluence) {
-            CalPlatform::writeInteger(os, currentInfluence->boneId);
+            CalPlatform::writeInteger(os, (int)currentInfluence->boneId);
             CalPlatform::writeFloat(os, currentInfluence->weight);
 
             // check if an error happend
@@ -729,7 +729,7 @@ bool CalSaver::saveCoreTrack(std::ostream& file, const std::string& strFilename,
     }
 
     // Write the coreBoneId.
-    if (!CalPlatform::writeInteger(file, pCoreTrack->coreBoneId)) {
+    if (!CalPlatform::writeInteger(file, (int)pCoreTrack->coreBoneId)) {
         CalError::setLastError(CalError::FILE_WRITING_FAILED, __FILE__, __LINE__, strFilename);
         return false;
     }
@@ -817,6 +817,7 @@ bool CalSaver::saveXmlCoreSkeleton(const std::string& strFilename, CalCoreSkelet
         TiXmlElement bone("BONE");
         bone.SetAttribute("ID", boneId);
         bone.SetAttribute("NAME", pCoreBone->name);
+        bone.SetAttribute("NUMCHILDS", pCoreSkeleton->getChildIds(pCoreBone).size());
         if (pCoreBone->hasLightingData()) {
             bone.SetAttribute("LIGHTTYPE", pCoreBone->lightType);
             str.str("");
@@ -951,7 +952,7 @@ bool CalSaver::saveXmlCoreAnimation(std::ostream& os, CalCoreAnimation* pCoreAni
         const CalCoreTrack* pCoreTrack = &*iteratorCoreTrack;
 
         TiXmlElement track("TRACK");
-        track.SetAttribute("BONEID", pCoreTrack->coreBoneId);
+        track.SetAttribute("BONEID", (int)pCoreTrack->coreBoneId);
 
         // Always save out the TRANSLATIONREQUIRED flag in XML, and save the translations iff the flag is true.
         bool translationIsDynamic = pCoreTrack->translationIsDynamic;
@@ -1241,7 +1242,7 @@ bool CalSaver::saveXmlCoreMesh(std::ostream& os, CalCoreMesh* pCoreMesh) {
             for (; currentInfluence != nextVertex; ++currentInfluence) {
                 TiXmlElement influence("INFLUENCE");
 
-                influence.SetAttribute("ID", currentInfluence->boneId);
+                influence.SetAttribute("ID", (int)currentInfluence->boneId);
 
                 str.str("");
                 str << currentInfluence->weight;
@@ -1274,7 +1275,7 @@ bool CalSaver::saveXmlCoreMesh(std::ostream& os, CalCoreMesh* pCoreMesh) {
                 size_t blendId = bv.vertexId;
                 const CalCoreSubmesh::Vertex& Vertex = vectorVertex[blendId];
                 static float differenceTolerance = 1.0;
-                CalVector positionDiff = bv.position.asCalVector();
+                CalVector positionDiff = (bv.position-Vertex.position).asCalVector();
                 float positionDiffLength = positionDiff.length();
 
                 bool skip = positionDiffLength < differenceTolerance;
@@ -1291,8 +1292,8 @@ bool CalSaver::saveXmlCoreMesh(std::ostream& os, CalCoreMesh* pCoreMesh) {
                 str << positionDiffLength;
                 blendVert.SetAttribute("POSDIFF", str.str());
 
-                const CalVector4 p = Vertex.position + bv.position;
-                const CalVector4 n = Vertex.normal + bv.normal;
+                const CalVector4 p = /* Vertex.position + */ bv.position;
+                const CalVector4 n = /* Vertex.normal + */ bv.normal;
 
                 TiXmlElement pos("POSITION");
                 str.str("");
@@ -1311,7 +1312,8 @@ bool CalSaver::saveXmlCoreMesh(std::ostream& os, CalCoreMesh* pCoreMesh) {
                 for (size_t tcI = 0; tcI < vectorvectorTextureCoordinate.size(); tcI++) {
                     TiXmlElement tcXml("TEXCOORD");
                     str.str("");
-                    str << 0.0f << " " << 0.0f;
+                    const CalCoreSubmesh::TextureCoordinate& tc = vectorvectorTextureCoordinate[tcI][blendId];
+                    str << tc.u << " " << tc.v;
                     TiXmlText tcdata(str.str());
                     tcXml.InsertEndChild(tcdata);
                     blendVert.InsertEndChild(tcXml);
