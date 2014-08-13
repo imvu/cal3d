32997abf598c1feacff4fb11201fd399c9bec684
bliu <bliu@07c76cb3-cb09-0410-85de-c24d39f1912e>
2014-08-13 00:58:29 +0000

    SSR-521:Correct quaternions when they are read in, not when formatted for output
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@200339 07c76cb3-cb09-0410-85de-c24d39f1912e

--- src/cal3d/loader.cpp
+++ src/cal3d/loader.cpp
@@ -12,6 +12,7 @@
 #include "config.h"
 #endif
 
+#include <iostream>
 #include <boost/optional.hpp>
 #include <stdexcept>
 #include <rapidxml.hpp>
@@ -588,8 +589,8 @@ CalCoreBonePtr CalLoader::loadCoreBones(CalBufferSource& dataSrc, int version) {
     }
 
 
-    CalQuaternion rot(rx, ry, rz, rw);
-    CalQuaternion rotbs(rxBoneSpace, ryBoneSpace, rzBoneSpace, rwBoneSpace);
+    CalQuaternion rot(rx, ry, rz, -rw);
+    CalQuaternion rotbs(rxBoneSpace, ryBoneSpace, rzBoneSpace, -rwBoneSpace);
     CalVector trans(tx, ty, tz);
 
     // allocate a new core bone instance
@@ -699,7 +700,7 @@ CalCoreKeyframePtr CalLoader::loadCoreKeyframe(
         dataSrc.readFloat(rw);
     }
 
-    CalCoreKeyframePtr pCoreKeyframe(new CalCoreKeyframe(time, t, CalQuaternion(rx, ry, rz, rw)));
+    CalCoreKeyframePtr pCoreKeyframe(new CalCoreKeyframe(time, t, CalQuaternion(rx, ry, rz, -rw)));
     return pCoreKeyframe;
 }
 
--- src/cal3d/quaternion.h
+++ src/cal3d/quaternion.h
@@ -82,7 +82,7 @@ public:
     void setAxisAngle(const CalVector& axis, float angle) {
         float h2 = angle * 0.5f;
         float s = sin(h2);
-        w = -cos(h2); // left-handed
+        w = cos(h2); 
         x = s * axis.x;
         y = s * axis.y;
         z = s * axis.z;
