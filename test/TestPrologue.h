96465eb252f71ddbb0247ec9fc3d29c0a32cb51e
jlee <jlee@07c76cb3-cb09-0410-85de-c24d39f1912e>
2014-11-19 23:44:29 +0000

    cal3d test: Add ability to approximately compare vectors of points
    git-svn-id: svn+ssh://svn.corp.imvu.com/home/svnrepos/trunk/stable/Source/cal3d@207206 07c76cb3-cb09-0410-85de-c24d39f1912e

--- test/TestPrologue.h
+++ test/TestPrologue.h
@@ -2,6 +2,7 @@
 
 #include <TestFramework/TestFramework.h>
 #include <cal3d/tinyxml.h>
+#include <cal3d/vector4.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/lexical_cast.hpp>
@@ -11,3 +12,17 @@ using boost::scoped_ptr;
 using boost::shared_ptr;
 using boost::lexical_cast;
 using boost::scoped_array;
+
+inline bool AreClose(
+    const CalPoint4& p1,
+    const CalPoint4& p2,
+    float tolerance
+) {
+    return (p1.asCalVector4() - p2.asCalVector4()).length() < tolerance;
+}
+
+namespace std { // :(
+    inline std::ostream& operator<<(std::ostream& os, const CalPoint4& v) {
+        return os << "[" << v.x << ", " << v.y << ", " << v.z << "]";
+    }
+}
